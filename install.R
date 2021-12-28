e2cd1583a2d18ef582864b41701c30e00c277378
Ryan Wesslen <rhymenoceros@MacBook-Air-3.attlocal.net>
2021-12-28 11:24:51 -0500

    modify install.R to install current renv

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 install.packages('remotes', repos = c(CRAN = 'https://cloud.r-project.org'))
-remotes::install_github('rstudio/renv@0.14.0')
+remotes::install_github('rstudio/renv')
 
 renv::restore()
